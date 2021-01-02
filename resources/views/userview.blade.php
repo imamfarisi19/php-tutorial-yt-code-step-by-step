5bfa2cbd13850ab8b3ee5309fdc9a4d9d2a3dd68
imamfarisi19 <edwardindo1@gmail.com>
2021-01-02 23:42:53 +0800

    first commit

---
+++ resources/views/userview.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>From</title>
+    </head>
+    <body>
+        <form action="" method="POST">
+        <input type="text" name="user">
+        <br><br>
+        <input type="text" name="email">
+        <br><br>
+        <button type="submit">Submit</button>
+        </form>
+    </body>
+</html>
