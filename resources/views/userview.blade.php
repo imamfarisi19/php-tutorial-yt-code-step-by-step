ea0881841396a1b4e0590aae8aede0fd04eb3853
imamfarisi19 <edwardindo1@gmail.com>
2021-01-03 00:07:56 +0800

    interface input of user and email and subbmit button

--- resources/views/userview.blade.php
+++ resources/views/userview.blade.php
@@ -3,8 +3,9 @@
         <title>From</title>
     </head>
     <body>
-        <form action="" method="POST">
+        <form action="userController" method="POST">
         <input type="text" name="user">
+        {{csrf_field}}
         <br><br>
         <input type="text" name="email">
         <br><br>
